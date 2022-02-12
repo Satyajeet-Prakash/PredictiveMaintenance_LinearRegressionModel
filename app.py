db577c6d7bb1a9ef2c32875f855a554716230777
Satyajeet <sant8430@gmail.com>
2022-02-12 22:09:36 +0530

    refactoring

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 @cross_origin()
-def homePage():
+def homepage():
     return  render_template('index.html')
 
 @app.route('/predict',methods=['POST','GET'])
